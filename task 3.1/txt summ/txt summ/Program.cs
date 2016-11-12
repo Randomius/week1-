af2f835a66ada2faada6ca461810479d09306663
Anton Andrushchenko <anton.andrushchenko.work@gmail.com>
2016-11-12 10:31:22 +0200

    summ of numbers in txt
    all txt to string, then split , then summ

---
+++ task 3.1/txt summ/txt summ/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace txt_summ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input file pass:");
+            string fName = Console.ReadLine();
+            FileInfo fi = new FileInfo(fName);
+            string txt = File.ReadAllText(fName);
+            string[] mas = new string[] { " ", "\r", "\n" };
+            string[] result;
+
+            result = txt.Split(mas, StringSplitOptions.None);
+
+            int kol = 0;
+            int sum = 0;
+            
+            if (!fi.Exists)
+            {
+                Console.WriteLine("File not exist.");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            foreach (string s in result)
+            {
+                try
+                {
+
+
+                    {
+
+                        kol++;
+                        sum += Convert.ToInt32(s);
+
+
+                    }
+
+                    
+
+
+
+                }
+                catch (FormatException)
+                {
+
+                }
+            }
+            Console.WriteLine("sum of numbers" + sum);
+            Console.ReadKey();
+        }
+    }
+}
